96b4a4803bcca9ef575ece7b7f1d39df681593e7
svobodapraha <vlada.svoboda@volny.cz>
2017-04-17 16:53:28 +0200

    writing to real port on pi

--- MyTest485/mainwindow.cpp
+++ MyTest485/mainwindow.cpp
@@ -8,13 +8,20 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     RS485Bus = new QSerialPort(this);
     RS485Bus->setPortName("/dev/ttyS0");
+    RS485Bus->setBaudRate(QSerialPort::Baud115200);
+    RS485Bus->setDataBits(QSerialPort::Data8);
+    RS485Bus->setParity(QSerialPort::NoParity);
+    RS485Bus->setStopBits(QSerialPort::OneStop);
+    RS485Bus->setFlowControl(QSerialPort::NoFlowControl);
     RS485Bus->open(QIODevice::ReadWrite);
+    qDebug()<<RS485Bus->errorString();
 
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    RS485Bus->close();
 }
 
 void MainWindow::on_Btn_TestWrite_clicked()
